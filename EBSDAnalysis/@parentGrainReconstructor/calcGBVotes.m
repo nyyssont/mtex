b6376253e70429b6f601d0f3f5bd333c274016f1
Tuomo Nyyssönen <tuomonyyssonen@hotmail.com>
2020-12-04 15:31:42 +0100

    Update calcGBVotes.m
    edited for cases where there are no starting parent grains

--- EBSDAnalysis/@parentGrainReconstructor/calcGBVotes.m
+++ EBSDAnalysis/@parentGrainReconstructor/calcGBVotes.m
@@ -3,20 +3,20 @@ function job = calcGBVotes(job,varargin)
 numFit = get_option(varargin,'numFit',2);
 
 % parent-child - votes
+if ~isempty(job.parentGrains)
+  grainPairs = neighbors(job.parentGrains, job.childGrains);
 
-grainPairs = neighbors(job.parentGrains, job.childGrains);
+  % extract the corresponding mean orientations
+  oriParent = job.grains('id',grainPairs(:,1) ).meanOrientation;
+  oriChild  = job.grains('id', grainPairs(:,2) ).meanOrientation;
 
-% extract the corresponding mean orientations
-oriParent = job.grains('id',grainPairs(:,1) ).meanOrientation;
-oriChild  = job.grains('id', grainPairs(:,2) ).meanOrientation;
+  % compute for each parent/child pair of grains the best fitting parentId
+  [parentId, fit] = calcParent(oriChild,oriParent,job.p2c,'numFit',numFit,'id');
 
-% compute for each parent/child pair of grains the best fitting parentId
-[parentId, fit] = calcParent(oriChild,oriParent,job.p2c,'numFit',numFit,'id');
-
-% store as table
-job.votes = table(grainPairs(:,2), parentId, fit, ...
-  'VariableNames',{'grainId','parentId','fit'});
-
+  % store as table
+  job.votes = table(grainPairs(:,2), parentId, fit, ...
+    'VariableNames',{'grainId','parentId','fit'});
+end
 
 % child-child - votes
 
